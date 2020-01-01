24b05d8af69851358308f8e70d9a79f271ec9818
pamoda95 <pamoda.15@cse.mrt.ac.lk>
2020-01-01 10:50:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/venturemate/UserDetails.java
@@ -0,0 +1,8 @@
+package com.example.venturemate;
+
+
+public class UserDetails {
+    static String username = "pamoda";
+    static String password = "";
+    static String chatWith = "shanika";
+}
